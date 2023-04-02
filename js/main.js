80e29f1d016bf2b3f72a8eae8c48623f00ed6a79
Daniyar Otegen <daniyarotegen@gmail.com>
2023-04-02 14:51:15 +0600

    Второе задание

--- js/main.js
+++ js/main.js
@@ -1,8 +1,27 @@
 console.log("Задание 1")
+
 console.log(document.getElementById('string-3').innerText)
 console.log(document.getElementById('string-5').innerText)
 console.log(document.getElementById('string-2').innerText)
 console.log(document.getElementById('string-6').innerText)
 console.log(document.getElementById('string-4').innerText)
 console.log(document.getElementById('string-1').innerText)
+
+
+console.log("Задание 2")
+
+let elements = document.getElementsByClassName('element')
+
+let i = 0
+for(element of elements){
+    i++
+    if(i<4) {
+        element.style.color = 'red'
+    }
+    else {
+        element.style.color = 'green'
+    }
+    console.log(element.innerText)
+}
+
 

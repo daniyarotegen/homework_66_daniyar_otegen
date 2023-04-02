28efb8e947bdbeadf232f5043adf7bdb54599a30
Daniyar Otegen <daniyarotegen@gmail.com>
2023-04-02 13:39:51 +0600

    Первое задание

---
+++ js/main.js
@@ -0,0 +1,8 @@
+console.log("Задание 1")
+console.log(document.getElementById('string-3').innerText)
+console.log(document.getElementById('string-5').innerText)
+console.log(document.getElementById('string-2').innerText)
+console.log(document.getElementById('string-6').innerText)
+console.log(document.getElementById('string-4').innerText)
+console.log(document.getElementById('string-1').innerText)
+
